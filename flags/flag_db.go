7e20a0b5436a832f8bc63f9d6fd62cb62956ded1
NaseemLi <naseem0922@proton.me>
2025-04-02 18:01:23 +0800

    feat-表配置

--- flags/flag_db.go
+++ flags/flag_db.go
@@ -11,6 +11,18 @@ func FlagDB() {
 	err := global.DB.AutoMigrate(
 		&models.UserModel{},
 		&models.UserConfModel{},
+		&models.ArticleModel{},
+		&models.CategoryModel{},
+		&models.ArticleDiggModel{},
+		&models.BannerModel{},
+		&models.CollectModel{},
+		&models.CommentModel{},
+		&models.GlobalNotificationModel{},
+		&models.ImageModel{},
+		&models.Model{},
+		&models.UserLoginModel{},
+		&models.UserTopArticleModel{},
+		&models.UserArticleLookHistoryModel{},
 	)
 	if err != nil {
 		logrus.Errorf("数据库迁移失败 %s", err)
