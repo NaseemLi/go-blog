b7dc36d31fb8b3a666b2c65d12ac786bab120eea
NaseemLi <naseem0922@proton.me>
2025-04-05 21:32:02 +0800

    feat-响应封装.1

--- api/site_api/enter.go
+++ api/site_api/enter.go
@@ -1,10 +1,7 @@
 package siteapi
 
 import (
-	"fmt"
-	"goblog/models/enum"
-	logservice "goblog/service/log_service"
-	"time"
+	"goblog/common/res"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,13 +10,8 @@ type Siteapi struct {
 }
 
 func (Siteapi) SiteInfoView(c *gin.Context) {
-	fmt.Println("1")
-	logservice.NewLoginSuccess(c, enum.UserPwdLoginType)
-	logservice.NewLoginFail(c, enum.UserPwdLoginType, "用户不存在", "fengfeng", "1234")
-	c.JSON(200, gin.H{
-		"code": 0,
-		"msg":  "站点信息",
-	})
+	res.OkWithData("xx", c)
+	return
 }
 
 type SiteUpdateRequest struct {
@@ -27,27 +19,13 @@ type SiteUpdateRequest struct {
 }
 
 func (Siteapi) SiteUpdateView(c *gin.Context) {
-	log := logservice.Getlog(c)
-	log.ShowRequest()
-	log.ShowRequestHeader()
-	log.ShowResponseHeader()
-	log.SetImage("/xxx/xxx")
-	log.SetLink("yaml学习地址", "https://www.fengfengzhidao.com")
-	log.ShowResponse()
-	log.SetTitle("更新站点")
-	log.SetItemInfo("请求时间", time.Now())
-
+	//log := logservice.Getlog(c)
 	var cr SiteUpdateRequest
 	err := c.ShouldBindJSON(&cr)
 	if err != nil {
-		log.SetError("参数绑定失败", err)
+		res.FailWithMsg(err.Error(), c)
+		return
 	}
-	log.SetItemInfo("结构体", cr)
 
-	id := log.Save()
-	c.JSON(200, gin.H{
-		"code": 0,
-		"msg":  "站点信息",
-		"data": id,
-	})
+	res.OkWithMsg("更新成功", c)
 }
---
+++ common/res/enter.go
@@ -0,0 +1,61 @@
+package res
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Code int
+
+const (
+	SuccessCode     Code = 0
+	FailValidCode   Code = 1001
+	FailServiceCode Code = 1002
+)
+
+func (c Code) String() string {
+	switch c {
+	case SuccessCode:
+		return "成功"
+	case FailValidCode:
+		return "校验失败"
+	case FailServiceCode:
+		return "服务异常"
+	}
+	return ""
+}
+
+var empty = map[string]any{}
+
+type Response struct {
+	Code Code   `json:"code"`
+	Data any    `json:"data"`
+	Msg  string `json:"msg"`
+}
+
+func (r Response) Json(c *gin.Context) {
+	c.JSON(200, r)
+}
+
+func Ok(data any, msg string, c *gin.Context) {
+	Response{SuccessCode, data, msg}.Json(c)
+}
+
+func OkWithData(data any, c *gin.Context) {
+	Response{SuccessCode, data, "成功"}.Json(c)
+}
+
+func OkWithMsg(msg string, c *gin.Context) {
+	Response{FailValidCode, empty, msg}.Json(c)
+}
+
+func FailWithMsg(msg string, c *gin.Context) {
+	Response{FailServiceCode, empty, msg}.Json(c)
+}
+
+func FailWithData(data any, msg string, c *gin.Context) {
+	Response{SuccessCode, data, msg}.Json(c)
+}
+
+func FailWithCode(code int, c *gin.Context) {
+	Response{Code(code), empty, Code(code).String()}.Json(c)
+}
