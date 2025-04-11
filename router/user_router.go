4fc8ebcfe0f96b43ae4226271e4e4e25f8abf747
NaseemLi <naseem0922@proton.me>
2025-04-11 13:12:28 +0800

    feat-图形验证码中间件

--- router/user_router.go
+++ router/user_router.go
@@ -2,11 +2,12 @@ package router
 
 import (
 	"goblog/api"
+	"goblog/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UserRouter(r *gin.RouterGroup) {
 	app := api.App.UserApi
-	r.POST("user/send_email", app.SendEmailView)
+	r.POST("user/send_email", middleware.CaptchaMiddleware, app.SendEmailView)
 }
