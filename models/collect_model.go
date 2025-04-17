6ecbc1e665d76d5d29afdfbda77652e02a55c6ae
NaseemLi <naseem0922@proton.me>
2025-04-17 21:16:20 +0800

    feat-收藏夹CRUD

--- models/collect_model.go
+++ models/collect_model.go
@@ -3,11 +3,11 @@ package models
 
 type CollectModel struct {
 	Model
-	Title        string    `gorm:"size:32" json:"title"`
-	Abstract     string    `gorm:"size:256" json:"abstract"`
-	Cover        string    `gorm:"size:256" json:"cover"`
-	ArticleCount int       `json:"articleCount"`
-	UserID       uint      `json:"userID"`
-	IsDefault    bool      `json:"isDefault"` //是否默认收藏夹
-	UserModel    UserModel `gorm:"foreignKey:UserID" json:"-"`
+	Title       string                    `gorm:"size:32" json:"title"`
+	Abstract    string                    `gorm:"size:256" json:"abstract"`
+	Cover       string                    `gorm:"size:256" json:"cover"`
+	ArticleList []UserArticleCollectModel `gorm:"foreignKey:CollectID" json:"-"`
+	UserID      uint                      `json:"userID"`
+	IsDefault   bool                      `json:"isDefault"` //是否默认收藏夹
+	UserModel   UserModel                 `gorm:"foreignKey:UserID" json:"-"`
 }
