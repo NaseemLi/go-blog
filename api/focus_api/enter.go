2fea35be24f6851e0a2a545118362d103d68eb22
NaseemLi <naseem0922@proton.me>
2025-04-21 14:23:54 +0800

    feat-我的关注列表

--- api/focus_api/enter.go
+++ api/focus_api/enter.go
@@ -1,11 +1,13 @@
 package focusapi
 
 import (
+	"goblog/common"
 	"goblog/common/res"
 	"goblog/global"
 	"goblog/middleware"
 	"goblog/models"
 	"goblog/utils/jwts"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,7 +19,7 @@ type FocusUserRequest struct {
 	FocusUserID uint `json:"focusUserID" binding:"required"`
 }
 
-func (FocusApi) FocusUserApi(c *gin.Context) {
+func (FocusApi) FocusUserView(c *gin.Context) {
 	cr := middleware.GetBind[FocusUserRequest](c)
 	claims := jwts.GetClaims(c)
 
@@ -38,7 +40,8 @@ func (FocusApi) FocusUserApi(c *gin.Context) {
 	//查之前是否已经关注
 	var userFocus models.UserFocusModel
 	err = global.DB.Take(&userFocus, "user_id = ? and focus_user_id = ?", claims.UserID, cr.FocusUserID).Error
-	if err != nil {
+	if err == nil {
+		// 查到了记录，说明已经关注
 		res.FailWithMsg("请勿重复关注", c)
 		return
 	}
@@ -50,4 +53,42 @@ func (FocusApi) FocusUserApi(c *gin.Context) {
 	})
 
 	res.OkWithMsg("关注成功", c)
+}
+
+type FocusUserListRequest struct {
+	common.PageInfo
+	FocusUserID uint `form:"focusUserID"`
+}
+
+type FocusUserListResponse struct {
+	FocusUserID       uint      `json:"focusUserID"`
+	FocusUserNickname string    `json:"focusUserNickname"`
+	FocusUserAvatar   string    `json:"focusUserAvatar"`
+	FocusUserAbstract string    `json:"focusUserAbstract"`
+	CreateAt          time.Time `json:"createAt"`
+}
+
+func (FocusApi) FocusUserListView(c *gin.Context) {
+	cr := middleware.GetBind[FocusUserListRequest](c)
+	claims := jwts.GetClaims(c)
+
+	_list, _, _ := common.ListQuery(models.UserFocusModel{
+		FocusUserID: cr.FocusUserID,
+		UserID:      claims.UserID,
+	}, common.Options{
+		PageInfo: cr.PageInfo,
+		Preloads: []string{"FocusUserModel"},
+	})
+
+	var list = make([]FocusUserListResponse, 0)
+	for _, model := range _list {
+		list = append(list, FocusUserListResponse{
+			FocusUserID:       model.FocusUserModel.ID,
+			FocusUserNickname: model.FocusUserModel.Nickname,
+			FocusUserAvatar:   model.FocusUserModel.Avatar,
+			FocusUserAbstract: model.FocusUserModel.Abstract,
+			CreateAt:          model.CreatedAt,
+		})
+	}
+	res.OkWithList(list, len(list), c)
 }
