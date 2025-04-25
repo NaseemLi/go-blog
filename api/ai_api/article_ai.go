240c5374d0e9b5873ca1160af05e9b9079b9de98
NaseemLi <naseem0922@proton.me>
2025-04-26 00:17:16 +0800

    feat-文章Ai接口

---
+++ api/ai_api/article_ai.go
@@ -0,0 +1,66 @@
+package aiapi
+
+import (
+	"context"
+	"encoding/json"
+	"goblog/common/res"
+	"goblog/global"
+	"goblog/middleware"
+	"goblog/models"
+	aiservice "goblog/service/ai_service"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/olivere/elastic/v7"
+
+	"github.com/sirupsen/logrus"
+)
+
+type ArticleAiRequest struct {
+	Content string `json:"content" binding:"required"`
+}
+
+func (AiApi) ArticleAiView(c *gin.Context) {
+	cr := middleware.GetBind[ArticleAiRequest](c)
+	// 查这个内容关联的文章列表
+	if !global.Config.Ai.Enable {
+		res.FailWithMsg("AI功能未开启", c)
+		return
+	}
+
+	// 查这个内容关联的文章列表
+	query := elastic.NewBoolQuery()
+	query.Should(
+		elastic.NewMatchQuery("title", cr.Content),
+		elastic.NewMatchQuery("abstract", cr.Content),
+		elastic.NewMatchQuery("content", cr.Content),
+	)
+	// 只能查发布的文章
+	query.Must(elastic.NewTermQuery("status", 3))
+	result, err := global.ESClient.
+		Search(models.ArticleModel{}.Index()).
+		Query(query).
+		From(1).
+		Size(10).
+		Do(context.Background())
+	if err != nil {
+		source, _ := query.Source()
+		byteData, _ := json.Marshal(source)
+		logrus.Errorf("查询失败 %s \n %s", err, string(byteData))
+		res.FailWithMsg("查询失败", c)
+		return
+	}
+	var list []string
+	for _, hit := range result.Hits.Hits {
+		list = append(list, string(hit.Source))
+	}
+	content := "[" + strings.Join(list, ",") + "]"
+	msgChan, err := aiservice.ChatStream(cr.Content, content)
+	if err != nil {
+		res.FailWithMsg("ai分析失败", c)
+		return
+	}
+	for s := range msgChan {
+		res.OkWithData(s, c)
+	}
+}
