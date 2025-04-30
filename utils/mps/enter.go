b41d01b20890f1b7da0fbb09d4cb0775287e6a70
NaseemLi <naseem0922@proton.me>
2025-04-30 22:56:59 +0800

    fix-用户标签更新失败

--- utils/mps/enter.go
+++ utils/mps/enter.go
@@ -1,6 +1,9 @@
 package mps
 
-import "reflect"
+import (
+	"encoding/json"
+	"reflect"
+)
 
 func Struct2map(data any, t string) (mp map[string]any) {
 	mp = make(map[string]any)
@@ -15,7 +18,14 @@ func Struct2map(data any, t string) (mp map[string]any) {
 			continue
 		}
 		if val.Kind() == reflect.Ptr {
-			mp[tag] = val.Elem().Interface()
+			v1 := val.Elem().Interface()
+			if val.Elem().Kind() == reflect.Slice {
+				byteData, _ := json.Marshal(v1)
+				mp[tag] = string(byteData)
+			} else {
+				mp[tag] = v1
+			}
+
 			continue
 		}
 		mp[tag] = val.Interface()
