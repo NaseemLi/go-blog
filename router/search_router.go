47f3a9268791271a16eadfb6033f9b5b28fa48e2
NaseemLi <naseem0922@proton.me>
2025-05-03 00:04:10 +0800

    feat-es查标签聚合列表

--- router/search_router.go
+++ router/search_router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"goblog/api"
 	searchapi "goblog/api/search_api"
+	"goblog/common"
 	"goblog/middleware"
 
 	"github.com/gin-gonic/gin"
@@ -11,5 +12,6 @@ import (
 func SearchRouter(r *gin.RouterGroup) {
 	app := api.App.SearchApi
 	r.GET("search/article", middleware.BindQueryMiddleware[searchapi.ArticleSearchRequest], app.ArticleSearchView)
+	r.GET("search/tags", middleware.BindQueryMiddleware[common.PageInfo], app.TagAggView)
 	r.GET("search/text", middleware.BindQueryMiddleware[searchapi.TextSearchRequest], app.TextSearchView)
 }
